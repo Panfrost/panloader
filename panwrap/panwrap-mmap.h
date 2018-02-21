363836fcd019a15605fad3c92a0b3ed7aa352940
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2018-02-21 00:24:57 +0000

    Let it breathe

--- panwrap/panwrap-mmap.h
+++ panwrap/panwrap-mmap.h
@@ -47,7 +47,8 @@ struct panwrap_mapped_memory {
 
 #define TOUCH_LEN(mem, addr, sz, ename, number) \
 	memset(mem->touched + ((addr - mem->gpu_va) / sizeof(uint32_t)), 1, sz / sizeof(uint32_t)); \
-	panwrap_log("memcpy(%s + %d, &%s_%d, sizeof(%s_%d));\n", mem->name, (int) ((addr - mem->gpu_va) / sizeof(uint32_t)), ename, number, ename, number);
+	panwrap_log("\n"); \
+	panwrap_log("memcpy(%s + %d, &%s_%d, sizeof(%s_%d));\n\n", mem->name, (int) ((addr - mem->gpu_va) / sizeof(uint32_t)), ename, number, ename, number);
 
 #define TOUCH(mem, addr, obj, ename, number) \
 	TOUCH_LEN(mem, addr, sizeof(obj), ename, number)
