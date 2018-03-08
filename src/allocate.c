cbaec7ce016c5859e7b7cd68d049f323c00fe549
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2018-03-08 23:31:35 +0000

    Simple watermark alloactor

---
+++ src/allocate.c
@@ -0,0 +1,46 @@
+/*
+ * © Copyright 2018 The Panfrost Community
+ *
+ * This program is free software and is provided to you under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation, and any use by you of this program is subject to the terms
+ * of such GNU licence.
+ *
+ * A copy of the licence is included with the program, and can also be obtained
+ * from Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pandev.h"
+
+// TODO: An actual allocator, perhaps
+// TODO: Multiple stacks for multiple bases?
+
+off_t stack_bottom = 0;
+
+off_t
+pandev_allocate_offset(off_t *stack, size_t sz)
+{
+	off_t ret = *stack;
+	*stack += sz;
+	return ret;
+}
+
+
+void
+pandev_upload(mali_ptr base, uint32_t *base_map, void *data, size_t sz)
+{
+	/* Allocate space for the new GPU object */
+	off_t offset = pandev_allocate_offset(&stack_bottom, sz);
+
+	/* Upload it */
+	memcpy(base_map + offset, data, sz);
+
+	/* Return the GPU address */
+	return base + offset;
+}
+
