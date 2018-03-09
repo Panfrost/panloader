a5733906f89e35049f3c5af3fbc14aec5cdb40ba
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2018-03-09 01:57:01 +0000

    Remove ugly hack from git

--- build-panwrap.sh
+++
@@ -1,2 +0,0 @@
-#cp ../cwabbotts-open-gpu-tools/common/libcommon.so /dev/shm
-gcc -Iinclude/ -Ibuild/include -I../cwabbotts-open-gpu-tools/common/include panwrap/*.c -mfp16-format=ieee -o panwrap.so -ldl -lm -pthread ../cwabbotts-open-gpu-tools/common/libcommon.a -fpic -shared
